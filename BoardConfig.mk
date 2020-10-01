04cfe59bf5eb189dcfce47a5818625003bbc4c0e
LuK1337 <priv.luk@gmail.com>
2020-10-01 14:13:46 +0200

    sweet: power-libperfmgr: Allow device specific hooks for setMode, isModeSupported
    * Some devices may want to implement custom hooks
    
    Change-Id: Icb2d66471ec649a69b1e69849fd86282775052cb
    Signed-off-by: UnmoveD <unmoved2121@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -144,9 +144,6 @@ TARGET_COPY_OUT_VENDOR := vendor
 # Platform
 TARGET_BOARD_PLATFORM := sm6150
 
-# Power
-TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
-
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/configs/props/odm.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
