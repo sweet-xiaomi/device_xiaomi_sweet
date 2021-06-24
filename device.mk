aaa9e858b297b970d7bc092146ea52658584183b
Pulkit077 <pulkitagarwal2k1@gmail.com>
2021-06-24 18:14:24 +0530

    sweet: power-libperfmgr: mark powerhint.json as required and build as module
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>
    Signed-off-by: UnmoveD <unmoved2121@gmail.com>

--- device.mk
+++ device.mk
@@ -348,9 +348,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-sweet-libperfmgr
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/power-libperfmgr/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
-
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-3.9.1-vendorcompat \
