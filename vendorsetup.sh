216d56127d8c5e44ebf4e356cb323cb93516b69e
UnmoveD <unmoved2121@gmail.com>
2023-11-12 22:58:37 +0000

    sweet: add vendorsetup.sh
    Signed-off-by: UnmoveD <unmoved2121@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+# Hardware
+git clone https://github.com/LineageOS/android_hardware_lineage_compat.git -b lineage-21 hardware/lineage/compat
+
+# VİPER4AndroidFX
+git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX -b v4a packages/apps/ViPER4AndroidFX
+
+# MiuiCam
+git clone https://github.com/unmoved21/vendor_xiaomi_sweet-miuicamera -b fourteen vendor/xiaomi/sweet-miuicamera
