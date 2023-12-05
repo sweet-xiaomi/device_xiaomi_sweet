a502f4c8422ee68d8ca523053e1defde490529bd
UnmoveD <unmoved2121@gmail.com>
2023-12-05 14:20:20 +0000

    sweet: update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,8 +4,8 @@ git clone https://github.com/LineageOS/android_hardware_lineage_compat.git -b li
 # VİPER4AndroidFX
 git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX -b v4a packages/apps/ViPER4AndroidFX
 
-# MiuiCam
-git clone https://github.com/unmoved21/vendor_xiaomi_sweet-miuicamera -b fourteen vendor/xiaomi/sweet-miuicamera
+# Leica
+git clone https://gitlab.com/unmoved21/sweet-miuicamera -b leica14 vendor/xiaomi/sweet-miuicamera
 
 # Clang
 git clone https://gitlab.com/itsshashanksp/android_prebuilts_clang_host_linux-x86_clang-r498229b.git -b 13.0 prebuilts/clang/host/linux-x86/clang-r498229b
